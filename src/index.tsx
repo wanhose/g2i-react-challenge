c232b9d3ae4e7fab4d46b221973447a0dff1f1a2
wanhose <wanhose.development@gmail.com>
2021-10-16 16:49:47 +0200

    chore: initial setup

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "containers/App";
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
